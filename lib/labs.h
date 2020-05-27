6f933d447f48ab8ad8b055892c8b0a66617e3ce2
loig <loig.jezequel@univ-nantes.fr>
2020-05-27 15:58:51 +0200

    comments in lab.h

--- lib/labs.h
+++ lib/labs.h
@@ -1,12 +1,46 @@
+// labs provides basic utility functions to deal
+// with a player in a labyrinth.
 #include <stdbool.h>
 
-bool is_solvable();
-bool arrival_reached();
-void turn_left();
-void turn_right();
-bool is_move_forward_possible();
+// move_forward() moves the player of one step
+// in its current looking direction. Initially
+// the player looks up.
 void move_forward();
-void write_coord_terminal();
-void print_terminal();
 
+// turn_left() changes the lookind direction of
+// the player by rotating her of a quarter of a
+// round to the left.
+void turn_left();
+
+// turn_right() changes the lookind direction of
+// the player by rotating her of a quarter of a
+// round to the right.
+void turn_right();
+
+// is_move_forward_possible() tells whether or not
+// the player can move forward, that is, whether or
+// not there is a wall in front of the player. It
+// should always be used before the move_forward()
+// function.
+bool is_move_forward_possible();
+
+// arrival_reached() tells whether or not the
+// player is standing on the arrival of the
+// labyrinth.
+bool arrival_reached();
+
+// is_solvable() tells whether or not there exists
+// a path in the labyrinth from the starting position
+// to the arrival.
+bool is_solvable();
+
+// write_coord_terminal() displays the coordinates of
+// the current position of the player in the terminal.
+void write_coord_terminal();
+
+// print_terminal(s) displays the string s in the
+// terminal.
+void print_terminal(char* s);
+
+// to be explained later
 void submit(bool);
